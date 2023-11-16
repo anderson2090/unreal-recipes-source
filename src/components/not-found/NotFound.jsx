1d4b08e57c730b3021128bfe0864eeef523b1c62
usama nadeem <usamanadeem1991@gmail.com>
2023-11-17 04:18:12 +0500

    added some styles

--- src/components/not-found/NotFound.jsx
+++ src/components/not-found/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './not-found.module.css'
 import Heading from '../heading/Heading'
 
 const NotFound = () => {
     return (
         <>
             <Heading />
-            NotFound
+            <p className={styles.notFound}>404</p>
         </>
     )
 }
